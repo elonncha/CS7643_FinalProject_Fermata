89f5624a2932520d1674c6852f2f8c09af6da387
Elon Cha <54544200+elonncha@users.noreply.github.com>
2021-04-27 00:28:33 -0400

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -7,7 +7,7 @@ import os
 def collect_ABCFormat_data(abc):
     '''
 
-    :param abc: an ABCFormat Object from music21
+    :param abc: an ABCFormat Object from music21 (nottingham dataset)
     :return: a list of songs with notes, beats, and beatStrength
     '''
     output = []
@@ -31,10 +31,48 @@ def collect_ABCFormat_data(abc):
                     note_list.append(n.notes[0].nameWithOctave)  # for Chord Symbol, we use the first note
 
         if accept_song:
-            dic['note'], dic['beat'], dic['beat_strength'] = note_list, [str(n.beat) for n in notes], [n.beatStrength
-                                                                                                         for n in notes]
+            dic['note'], dic['beat'], dic['beat_strength'] = note_list, [str(n.beat) for n in notes], [n.beatStrength for n in notes]
             output.append(dic)
 
     print('process completed! accept songs: {0}/{1}'.format(len(output), len(abc)))
 
     return (output)
+
+
+
+def collect_ABCFormat_data_single(abc):
+    '''
+
+    :param abc: an ABCFormat Object with SINGLE song from music21 (folk dataset)
+    :return: a list of songs with notes, beats, and beatStrength
+    '''
+
+    notes = abc.flat.notes
+    dic = {'note': [],
+            'beat': [],
+            'beat_strength': []}
+
+    for n in notes:
+        try:
+            dic['note'].append(n.nameWithOctave)
+            dic['beat'].append(str(n.beat))
+            dic['beat_strength'].append(n.beatStrength)
+        except:
+            dic = {'note': [],
+                   'beat': [],
+                   'beat_strength': []} # bad sample, return empty
+            break
+
+    return (dic)
+
+
+
+
+def padding(list):
+    '''
+    add <e> to the end of each sequence to make the sequence length consistent
+    :param list: a list of sequences (list) of different seq_length
+    :return: a list of sequences (list) of consistent seq_length
+
+    '''
+    max_length = max(len(x) for x in list)
