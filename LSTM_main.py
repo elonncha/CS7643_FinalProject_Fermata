8cdb8590cb0e8618fa1996225ec9b8bb1d43f46b
Elon Cha <54544200+elonncha@users.noreply.github.com>
2021-05-01 15:19:22 -0400

    migrate comment

--- LSTM_main.py
+++ LSTM_main.py
@@ -72,32 +72,3 @@ for i, ds_name in enumerate(ds_names):
 
 
 
-'''
-# get dataset split idx
-np.random.seed(0)
-train_percentage = 0.70
-rand_idx = np.random.randint(0, note_past.shape[0], note_past.shape[0])
-train_size = int(train_percentage * note_past.shape[0])
-
-train_idx = rand_idx[:train_size]
-val_idx = rand_idx[train_size:train_size+int((rand_idx.shape[0]-train_size)/2)]
-test_idx = rand_idx[int((rand_idx.shape[0]-train_size)/2):]
-ds_idxs = [train_idx, val_idx, test_idx]
-ds_name = ['train', 'val', 'test']
-
-# split dataset
-data_r = [note_past, note_future, measure_past, measure_future]
-for i, idx in enumerate(ds_idxs):
-    path = './dataset_split/'+ds_name[i]
-    with open(path, 'wb') as pickle_w:
-        note_past, note_future, measure_past, measure_future = [x[idx].numpy() for x in data_r] # for this temporarily converted tensor back to numpy
-        write = {b'note_past': note_past,
-                b'note_future': note_future,
-                b'measure_past': measure_past,
-                b'measure_future': measure_future} 
-        pickle.dump(write, pickle_w)
-    # open test
-    with open(path, 'rb') as pickle_r:
-        dict = pickle.load(pickle_r, encoding='bytes')
-        print(ds_name[i], dict[b'note_past'].shape)
-'''
