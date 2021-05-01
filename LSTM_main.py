11a57d446b9d7979db8199e219cb1514505124f4
Alison Bernice Ma <alisonbernicema@Alisons-MacBook-Pro.local>
2021-04-30 21:10:36 -0400

    write to pickle

--- LSTM_main.py
+++ LSTM_main.py
@@ -33,6 +33,25 @@ measure_future_val, measure_future_test = train_test_split(note_past_test,
                                                            measure_pass_test,
                                                            measure_future_test, test_size = 0.5)
 
+data_train = [note_past_train, note_future_train, measure_past_train, measure_future_train, note_target_train]
+data_val = [note_past_val, note_future_val, measure_past_val, measure_future_val, note_target_val]
+data_test = [note_past_test, note_future_test, measure_pass_test, measure_future_test, note_target_test]
+data = [data_train, data_val, data_test]
+ds_names = ['train', 'val', 'test']
+
+for i, ds_name in enumerate(ds_names):
+    path = './dataset_split/'+ds_name
+    with open(path, 'wb') as pickle_w:
+        write = {b'note_past': data[i][0],
+                b'note_future': data[i][1],
+                b'measure_past': data[i][2],
+                b'measure_future': data[i][3],
+                b'target': data[i][4]} 
+        pickle.dump(write, pickle_w)
+    # open test
+    with open(path, 'rb') as pickle_r:
+        dict = pickle.load(pickle_r, encoding='bytes')
+        print(ds_name, dict[b'target'])
 
 
 
