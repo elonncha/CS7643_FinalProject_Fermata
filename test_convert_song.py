32a3ca5fa35c94cfd79342e1cdee3252cb94c6dd
Alison Bernice Ma <alisonbernicema@Alisons-MacBook-Pro.local>
2021-05-04 19:37:26 -0400

    inpainting updates

--- test_convert_song.py
+++ test_convert_song.py
@@ -1,82 +1,39 @@
 import pickle
 from data_util import *
 
-# temporarily copied here to work on it before putting back in data_util
-# def reconstruct_song(song_id_val,
-#                      note_past_val, note_future_val, note_target_predicted, note_dic,
-#                      measure_past_val, measure_future_val, measure_mask_val):
+data = INPAINT(data_root='./dataset_split', ds_type='test')
+note_dic = data.get_note_dict()
 
-#     # load raw txt song data
-#     file_name = 'data/folk.txt'
-#     with open(file_name, 'r') as f:
-#         raw = f.readlines()
-#         f.close()
-#     song_raw = []
-#     for ite, line in enumerate(raw):
-#         if 'M:4/4' in line:
-#             song = [raw[ite-1], line, raw[ite+1], raw[ite+2]]
-#             song_raw.append(song)
-#         else:
-#             continue
-#     song_raw_val = [song_raw[idx] for idx in song_id_val]
-
-    # decode predictions
-
-    # for i in note_target_predicted:
-    #     print('i:\n \n',i)
-
-
-    # note_past_val, note_target_predicted, note_future_val = note_decoder(note_past_val, note_dic), \
-    #                                                         note_decoder(note_target_predicted, note_dic), \
-    #                                                         note_decoder(note_future_val, note_dic)
-    # # decode measure
-    # # measure_past_val = measure_past_val.tolist()
-    # measure_past_val = [[n for n in s if n>=0] for s in measure_past_val]
-    # # measure_future_val = measure_future_val.tolist()
-    # measure_future_val = [[n for n in s if n >= 0] for s in measure_future_val]
-    # print('measure: ', measure_past_val, measure_future_val)
-    # measure_val = [measure_past_val[i] + measure_mask_val[i] + measure_future_val[i] for i in range(len(song_id_val))]
-
-    # # reconstruct new songs
-    # note_predicted = []
-    # for i in range(len(song_id_val)):
-    #     new_song = note_past_val[i] + note_target_predicted[i] + note_future_val[i]
-    #     note_predicted.append(new_song)
-
-    # # write into raw txt format
-    # output = copy.deepcopy(song_raw_val)
-    # for i in range(len(song_raw_val)):
-    #     output[i][-1] = reverse_note_to_abc(note_predicted[i], measure_val[i])
-
-    # return(output)
-
-
-
-results_root = './results/Seq2Seq_LSTM/prediction'
-
+results_root = './results_01/Seq2Seq_LSTM/prediction'
 with open(results_root, 'rb') as pickle_r:
     dict = pickle.load(pickle_r, encoding='bytes')
     raw_score = dict[b'raw_score']
     note_target_pred = dict[b'abc']
-    print('target: ', note_target_pred, 'raw score: ', raw_score)
-print(len(note_target_pred))
-# did not shuffle data during training so works
-data = INPAINT(data_root='./dataset_split', ds_type='test')
 
-note_dic = data.get_note_dict()
+#i know the code is ugly
+songs = []
+ground_truth = []
+with open('./dataset_split/test', 'rb') as pickle_r:
+    dict = pickle.load(pickle_r, encoding='bytes')
+    past = note_decoder(dict[b'note_past'], note_dic)
+    future = note_decoder(dict[b'note_future'], note_dic)
+    target = note_decoder(dict[b'target'], note_dic)
+    predictions = note_decoder(np.argmax(raw_score, axis=2), note_dic)
+    for i in enumerate(note_target_pred):
+        # idk why the predictoin was a tuple
+        y = i[1]
+        for idx, j in enumerate(y):
+            x = np.delete(j[1:], np.argwhere(j[1:]=='<s>'))
+            x = np.delete(x, np.argwhere(x=='<e>'))
+            x = np.delete(x, np.argwhere(x=='</s>'))
+            song = np.concatenate((np.delete(past[idx], np.argwhere(past[idx]=='<e>')), x, np.delete(future[idx], np.argwhere(past[idx]=='<e>'))))
+            actual = np.concatenate((np.delete(past[idx], np.argwhere(past[idx]=='<e>')), target[idx], np.delete(future[idx], np.argwhere(past[idx]=='<e>'))))
+            songs.append(song)
+            ground_truth.append(actual)
 
-note_past, measure_past, \
-    note_future, measure_future, \
-        target, measure_mask, song_id = data.note_past, data.measure_past, \
-                                            data.note_future,data.measure_future, \
-                                                data.target, data.measure_mask, data.song_id
-
-note_past, measure_past, \
-    note_future, measure_future = [d.tolist() for d in [note_past, measure_past, note_future, measure_future]]    
-
-
-output = reconstruct_song(song_id,
-                            note_past, note_future, note_target_pred, note_dic,
-                            measure_past, measure_future, measure_mask)
-
-print(output)
+# test
+i=130
+print(songs[i])
+print(ground_truth[i])
+print((target[i]))
+print((predictions[i]))
